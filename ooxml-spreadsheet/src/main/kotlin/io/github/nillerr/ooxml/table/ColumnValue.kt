7634301cf249328859db902c8b2532947cf911c2
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2023-09-29 07:41:00 +0200

    Change ColumnValue to sealed class

--- ooxml-spreadsheet/src/main/kotlin/io/github/nillerr/ooxml/table/ColumnValue.kt
+++ ooxml-spreadsheet/src/main/kotlin/io/github/nillerr/ooxml/table/ColumnValue.kt
@@ -6,22 +6,22 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
 
-sealed interface ColumnValue
+sealed class ColumnValue
 
-object EmptyColumnValue : ColumnValue
+object EmptyColumnValue : ColumnValue()
 
-data class StringColumnValue(val value: String) : ColumnValue
+data class StringColumnValue(val value: String) : ColumnValue()
 
-data class NumberColumnValue(val value: Number) : ColumnValue
+data class NumberColumnValue(val value: Number) : ColumnValue()
 
-data class BooleanColumnValue(val value: Boolean) : ColumnValue
+data class BooleanColumnValue(val value: Boolean) : ColumnValue()
 
-data class InstantColumnValue(val value: Instant) : ColumnValue
+data class InstantColumnValue(val value: Instant) : ColumnValue()
 
-data class LocalDateColumnValue(val value: LocalDate) : ColumnValue
+data class LocalDateColumnValue(val value: LocalDate) : ColumnValue()
 
-data class LocalTimeColumnValue(val value: LocalTime) : ColumnValue
+data class LocalTimeColumnValue(val value: LocalTime) : ColumnValue()
 
-data class LocalDateTimeColumnValue(val value: LocalDateTime) : ColumnValue
+data class LocalDateTimeColumnValue(val value: LocalDateTime) : ColumnValue()
 
-data class HyperlinkColumnValue(val value: Hyperlink) : ColumnValue
+data class HyperlinkColumnValue(val value: Hyperlink) : ColumnValue()
